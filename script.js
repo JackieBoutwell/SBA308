28ea9d0c1402f34e93fee50354836eb5b9228d7a
Jackie Boutwell <boutwelljackie983@gmail.com>
2024-03-28 15:53:36 -0600

    added a try/catch

--- script.js
+++ script.js
@@ -76,77 +76,76 @@ const LearnerSubmissions = [
   },
 ];
 function getLearnerData(course, ag, submissions) {
-  let learners = [];
-  // let assignmentID = AssignmentGroup ['assignment_id']
-  // let points_possible =
+  try {  //this is my try/catch!
+    let learners = [];
+    // let assignmentID = AssignmentGroup ['assignment_id']
+    // let points_possible =
     //loop through the learn submissons and get the id and start building the array that contains objects.
 
-  //approach on
+    //approach on
     submissions.forEach((learnerObj) => {
-          let assignment_id = learnerObj.assignment_id
-          let score = learnerObj.submission.score
-          let found;
-            for (const iterator of AssignmentGroup['assignments']) {
-              if (iterator.id === assignment_id) found = iterator
-            }  
+      // try { 
+      let assignment_id = learnerObj.assignment_id
+      let score = learnerObj.submission.score
+      let found;
+      for (const iterator of AssignmentGroup['assignments']) {
+        if (iterator.id === assignment_id) found = iterator
+      }
 
       //find when assignment is late
       let dueDate = Date.parse(found.due_at)
-      let submissionDate =Date.parse( learnerObj.submission.submitted_at)
+      let submissionDate = Date.parse(learnerObj.submission.submitted_at)
       console.log("************")
       // when the submission date is newer than the duedate it is late
-      if(submissionDate>dueDate){
-        console.log('late',learnerObj.submission.submitted_at,found.due_at)
+      if (submissionDate > dueDate) {
+        console.log('late', learnerObj.submission.submitted_at, found.due_at)
         // deduct 10 percent of the total points
-        score = (score- found.points_possible*0.1)/found.points_possible 
+        score = (score - found.points_possible * 0.1) / found.points_possible
 
         //if submission date is older than due date do not count 
-      }else if (submissionDate<dueDate){
-        console.log("do not count ",learnerObj.submission.submitted_at,found.due_at)
+      } else if (submissionDate < dueDate) {
+        console.log("do not count ", learnerObj.submission.submitted_at, found.due_at)
       }
-      else{
-        console.log("right time",learnerObj.submission.submitted_at,found.due_at)
-        score = score/found.points_possible
+      else {
+        console.log("right time", learnerObj.submission.submitted_at, found.due_at)
+        score = score / found.points_possible
       }
-
-      //
-           
-
-      
-        if (!learners.find((item)=>item.id ===learnerObj.learner_id)) {
-          let obj ={ 
-            "id": learnerObj.learner_id,
-              [assignment_id]:score
-          }
-            learners.push(obj);
-        }else{
-          for (let i =0 ; i<learners.length;i++){
-           if(learners[i].id === learnerObj.learner_id){
-             learners[i][assignment_id] = score
-           }
+      if (!learners.find((item) => item.id === learnerObj.learner_id)) {
+        let obj = {
+          "id": learnerObj.learner_id,
+          [assignment_id]: score
+        }
+        learners.push(obj);
+      } else {
+        for (let i = 0; i < learners.length; i++) {
+          if (learners[i].id === learnerObj.learner_id) {
+            learners[i][assignment_id] = score
           }
         }
+      }
 
     });
     console.log(learners);
 
-  // here, we would process this data to achieve the desired result.
-  const result = [
-    {
-      id: 125,
-      avg: 0.985, // (47 + 150) / (50 + 150)
-      1: 0.94, // 47 / 50
-      2: 1.0, // 150 / 150
-    },
-    {
-      id: 132,
-      avg: 0.82, // (39 + 125) / (50 + 150)
-      1: 0.78, // 39 / 50
-      2: 0.833, // late: (140 - 15) / 150
-    },
-  ];
-  return result;
+    // here, we would process this data to achieve the desired result.
+    const result = [
+      {
+        id: 125,
+        avg: 0.985, // (47 + 150) / (50 + 150)
+        1: 0.94, // 47 / 50
+        2: 1.0, // 150 / 150
+      },
+      {
+        id: 132,
+        avg: 0.82, // (39 + 125) / (50 + 150)
+        1: 0.78, // 39 / 50
+        2: 0.833, // late: (140 - 15) / 150
+      },
+    ];
+    return result;
+  } catch (e) {console.log(e);}
 }
+
 // console.log(1 + 2);
 const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
 // first if else statment: const result2 = passOrFail();
